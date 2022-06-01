2e4cb1c0d1b6c0bf4889c7153137e19d631ac63f
ANour8 <103966421+ANour8@users.noreply.github.com>
2022-06-01 20:01:01 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "region" {
+  default     = "us-east-1"
+  description = "AWS region"
+}
+
+variable "private_subnets" {
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  description = "private subnet range"
+}
+
+variable "public_subnets" {
+  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  description = "public subnet range"
